89d1ce910f13eb0be7898ec6d6aa68762dc984b3
Chris Hales <91158102+halesc@users.noreply.github.com>
2022-08-16 14:18:55 -0700

    Update data exploration to included merged (inner jon) industry names.

--- code/00_data_exploration.R
+++ code/00_data_exploration.R
@@ -12,3 +12,9 @@ cps_data_file = "./data_raw/cps_00002.dat"
 # Contains metadata, nice to have as separate object
 cps_ddi = read_ipums_ddi(cps_ddi_file) 
 cps_data = read_ipums_micro(cps_ddi_file, data_file = cps_data_file)
+# Read in the Industry Codes
+industries = read_csv("./data_raw/indnames.csv")
+# Make colNames uppercase
+names(industries) <- toupper(names(industries))
+# Merge in Industry names
+merged_data = merge(cps_data, industries, by = "IND", all=FALSE)
